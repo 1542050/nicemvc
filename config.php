3a53f9b81165b6a0ff0386db1e8c219634f82080
Kim Bui <kimbui@Kims-MacBook-Air.local>
2016-08-01 10:01:07 +0700

    create php mvc project

---
+++ config.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * @Author: kimbui
+ * @Date:   2016-08-01 09:22:35
+ * @Last Modified by:   kimbui
+ * @Last Modified time: 2016-08-01 09:51:12
+ */
+
+return array(
+  'base_url' => '',
+  'database' => array(
+    'host' => '',
+    'username' => '',
+    'password' => '',
+    'databse_name' => '',
+  ),
+);
---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * @Author: kimbui
+ * @Date:   2016-08-01 09:46:35
+ * @Last Modified by:   kimbui
+ * @Last Modified time: 2016-08-01 09:51:16
+ */
+
+echo "string";
+?>
