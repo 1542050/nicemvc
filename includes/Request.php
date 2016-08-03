647388dc02ed730c5f41a634c57426c0bc4d1413
Kim Bui <kimbui@Kims-MacBook-Air.local>
2016-08-03 21:40:41 +0700

    update request method

--- includes/Request.php
+++ includes/Request.php
@@ -3,7 +3,7 @@
  * @Author: kimbui
  * @Date:   2016-08-01 17:56:47
  * @Last Modified by:   kimbui
- * @Last Modified time: 2016-08-02 08:03:24
+ * @Last Modified time: 2016-08-03 21:40:08
  */
 class Request {
   private $_param = array();
@@ -11,6 +11,53 @@ class Request {
   function __construct() {
     $this->setParam('controller', 'index');
     $this->setParam('action', 'index');
+  }
+
+  public function _getValue(&$array, $name) {
+    if ($name === NULL || $name == '') {
+      return $array;
+    }
+    if (isset($array[$name])) {
+      return $array[$name];
+    }
+    return NULL;
+  }
+
+  public function getQuery($name = NULL) {
+    return $this->_getValue($_GET, $name);
+  }
+
+  public function isQuery($name) {
+    if (isset($_GET[$name])) {
+      return TRUE;
+    }
+    return FALSE;
+  }
+
+  public function getPost($name = NULL) {
+    return $this->_getValue($_POST, $name);
+  }
+
+  public function isPost($name) {
+    if (isset($_POST[$name])) {
+      return TRUE;
+    }
+    return FALSE;
+  }
+
+  public function getSession($name = NULL) {
+    return $this->_getValue($_SESSION, $name);
+  }
+
+  public function setSession($name, $value) {
+    $_SESSION[$name] = $value;
+  }
+
+  public function isSession($name) {
+    if (isset($_SESSION[$name])) {
+      return $_SESSION[$name];
+    }
+    return FALSE;
   }
 
   public function setParam($name = NULL, $value = NULL) {
